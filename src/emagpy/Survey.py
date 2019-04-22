d6d9eb5a1a64ac5859d77aa1eea5d5d5ff379353
Guillaum <sagitta1618@gmail.com>
2019-04-22 16:31:01 +0100

    update

--- src/emagpy/Survey.py
+++ src/emagpy/Survey.py
@@ -6,23 +6,185 @@ Created on Tue Apr 16 20:27:12 2019
 @author: jkl
 """
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pykrige.ok import OrdinaryKriging
+from scipy.spatial.distance import cdist
+
+#%%
 
 class Survey(object):
     ''' Create a Survey object containing the raw EMI data.
+    Raw EMI file is
     '''
-    def __init__(self, fname):
-        self.df = pd.read_csv(fname)
+    def __init__(self, fname=None):
+        self.df = None # main dataframe
         self.freq = None # how to handle different frequencies ?
+        self.errorModel = None # a function that returns an error
+        self.sensor = None # sensor name (+ company)
+        self.coils = None # columns with the coils names and configuration
+        if fname is not None:
+            self.readFile(fname)
+    
+    def readFile(self, fname, sensor=None):
+        df = pd.read_csv(fname)
+        coils = []
+        for c in df.columns:
+            orientation = c[:3]
+            if ((orientation == 'VCP') | (orientation == 'VMD') | (orientation == 'PRP') |
+                    (orientation == 'HCP') | (orientation == 'HMD')):
+                # replace all orientation in HCP/VCP/PRP mode
+                if orientation == 'HMD':
+                    df = df.rename(columns={c:c.replace('HMD','VCP')})
+                if orientation == 'VMD':
+                    df = df.rename(columns={c:c.replace('VMD','HCP')})
+                coils.append(c)
+        self.coils = coils
+        self.freqs = df[coils].values[0,:] # first row for frequency (Hz)
+        self.hx = df[coils].values[1,:] # second for height (m)
+        self.df = df[2:]
+        self.sensor = sensor
+        # TODO if inphase is present we can parse it _inph or _quad
         
-    def show(self):
+        
+        
+    def show(self, coils='all', attr='ECa', ax=None):
         ''' Show the data.
         '''
+        if coils == 'all':
+            cols = self.coils
+        else:
+            cols = coils
+        
+        if ax is None:
+            fig, ax = plt.subplots()
+        else:
+            fig = ax.figure
+       
+        ax.plot(self.df[cols].values, 'o-')
+        ax.legend(cols)
+        ax.set_xlabel('Measurements')
+        ax.set_ylabel('Apparent Conductivity [mS/m]')
+        
+
+
+s = Survey('test/testFile1.csv')
+s.show()
+
+
+    #%%
+    def convertFromNMEA(self, targetProjection='EPSG:27700'): # British Grid 1936
+        ''' Convert NMEA string to selected CRS projection.
+        '''
+'''
+        def func(arg):
+    """ Convert NMEA string to WGS84 (GPS) decimal degree.
+    """
+    letter = arg[-1]
+    if (letter == 'W') | (letter == 'S'):
+        sign = -1
+    else:
+        sign = 1
+    arg = arg[:-1]
+    x = arg.index('.')
+    a = float(arg[:x-2]) # degree
+    b = float(arg[x-2:]) # minutes
+    return (a + b/60)*sign
+gps2deg = np.vectorize(func)
+
+
+wgs84=pyproj.Proj("+init=EPSG:4326") # LatLon with WGS84 datum used by GPS units and Google Earth
+osgb36=pyproj.Proj("+init=EPSG:27700") # UK Ordnance Survey, 1936 datum
+
+df['easting'], df['northing'] = pyproj.transform(wgs84, osgb36, 
+                                      df['lon'].values, df['lat'].values)
+
+'''
+
+
     
     def showMap(self):
         ''' Display a map of the measurements.
         '''
+'''
+    # clip with convexHull of data
+def clipConvexHull(xdata,ydata,x,y,z):
+    """ set to nan data outside the convex hull of xdata, yxdata
+    
+    Parameters:
+        xdata, ydata (arrays) : x and y position of the data collected
+        x, y (arrays) : x and y position of the computed interpolation points
+        z (arrays) : value of the interpolation
+    
+    Return:
+        znew (array) : a copy of z with nan when outside convexHull
+    """
+    knownPoints = np.array([xdata, ydata]).T
+    newPoints = np.array([x,y]).T
+    from scipy.spatial import ConvexHull
+    _, idx = np.unique(xdata+ydata, return_index=1)
+    q = ConvexHull(knownPoints[idx,:])
+    from scipy.spatial import Delaunay
+    hull = Delaunay(q.points[q.vertices, :])
+#    qq = q.points[q.vertices,:]
+        
+    znew = np.copy(z)
+    for i in range(0, len(z)):
+        if hull.find_simplex(newPoints[i,:])<0:
+            znew[i] = np.nan
+    return znew
+'''
+    
     
     def gridData(self):
         ''' TODO
         '''
+    
+    
+    def crossOverPoints(self):
+        ''' Build an error model based on the cross-over points.
+        '''
+'''       
+        modes = ['lo', 'hi']
+for i, couple in enumerate(dfs[:1]):
+    for k, df in enumerate(couple[:1]):
+        dist = cdist(df[['northing', 'easting']].values,
+                     df[['northing', 'easting']].values)
+        minDist = 1 # points at less than 1 m from each other are identical
+        ix, iy = np.where(((dist < minDist) & (dist > 0))) # 0 == same point
+        ifar = (ix - iy) > 200 # they should be at least 40 measuremens apart
+        ix, iy = ix[ifar], iy[ifar]
+        print('found', len(ix), '/', df.shape[0], 'crossing points')
         
+        # plot cross-over points
+        xcoord = df['easting'].values
+        ycoord = df['northing'].values
+        icross = np.unique(np.r_[ix, iy])
+        fig, ax = plt.subplots()
+        ax.set_title(dates[i] + ' ' + modes[k])
+        ax.plot(xcoord, ycoord, '.')
+        ax.plot(xcoord[icross], ycoord[icross], 'ro', label='crossing points')
+        ax.set_xlabel('Easing [m]')
+        ax.set_ylabel('Northing [m]')
+        fig.tight_layout()
+        fig.savefig(outputdir + 'crossOverPoints-' + dates[i] + modes[k] + '.png')
+#        fig.show()
+
+'''
+
+    
+    def gfCorrection(self):
+        ''' Apply the correction due to the 1m calibration.
+        '''
+        
+
+
+# test
+'''
+import a well formatted text file
+import two field dataset after selecting the instrument
+join lo and hi based on measurements numbers or regridding data
+
+'''
+
+#%%
--- src/ui.py
+++ src/ui.py
@@ -7,4 +7,427 @@ Created on Tue Apr 16 20:25:54 2019
 
 Main UI
 """
+import os
+import sys
+import time
 
+from PyQt5.QtWidgets import (QMainWindow, QSplashScreen, QApplication, QPushButton, QWidget,
+    QTabWidget, QVBoxLayout, QGridLayout, QLabel, QLineEdit, QMessageBox,
+    QFileDialog, QCheckBox, QComboBox, QTextEdit, QSlider, QHBoxLayout,
+    QTableWidget, QFormLayout, QTableWidgetItem, QHeaderView, QProgressBar,
+    QStackedLayout, QRadioButton, QGroupBox)#, QAction, QButtonGroup, QListWidget, QShortcut)
+from PyQt5.QtGui import QIcon, QPixmap, QIntValidator, QDoubleValidator#, QKeySequence
+from PyQt5.QtCore import QThread, pyqtSignal#, QProcess, QSize
+from PyQt5.QtCore import Qt
+
+#%% General crash ERROR
+import threading
+import traceback
+
+def errorMessage(etype, value, tb):
+    print('ERROR begin:')
+    traceback.print_exception(etype, value, tb)
+    print('ERROR end.')
+    errorMsg = traceback.format_exception(etype, value, tb,limit=None, chain=True)
+    finalError =''
+    for errs in errorMsg:
+        finalError += errs
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Critical)
+    msg.setText("<b>Critical error:</b>")
+    msg.setInformativeText('''Please see the detailed error below.<br>You can report the errors at:<p><a href='https://gitlab.com/hkex/pyr2/issues'>https://gitlab.com/hkex/pyr2/issues</a></p><br>''')
+    msg.setWindowTitle("Error!")
+#    msg.setWindowFlags(Qt.FramelessWindowHint)
+    msg.setDetailedText('%s' % (finalError))
+    msg.setStandardButtons(QMessageBox.Retry)
+    msg.exec_()
+
+def catchErrors():
+    sys.excepthook = errorMessage
+    threading.Thread.__init__
+
+
+#%%get relative path of images
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+class customThread(QThread):
+    ''' class needed to run out of main thread computation and then emit
+    signal to the main GUI thread to display message box.
+    '''
+    signal = pyqtSignal('PyQt_PyObject')
+
+    def __init__(self, func):
+        QThread.__init__(self)
+        self.func = func
+
+    def run(self):
+        output = self.func()
+        self.signal.emit(output) # inform the main thread of the output
+
+
+#%%
+QT_AUTO_SCREEN_SCALE_FACTOR = True # for high dpi display
+
+# small code to see where are all the directories
+frozen = 'not'
+if getattr(sys, 'frozen', False):
+        # we are running in a bundle
+        frozen = 'ever so'
+        bundle_dir = sys._MEIPASS
+else:
+        # we are running in a normal Python environment
+        bundle_dir = os.path.dirname(os.path.abspath(__file__))
+print( 'we are',frozen,'frozen')
+print( 'bundle dir is', bundle_dir )
+print( 'sys.argv[0] is', sys.argv[0] )
+print( 'sys.executable is', sys.executable )
+print( 'os.getcwd is', os.getcwd() )
+
+
+#%%
+class MatplotlibWidget(QWidget):
+    ''' Class to put matplotlibgraph into QWidget.
+    '''
+    def __init__(self, parent=None, figure=None, navi=False, itight=False, threed=False):
+        super(MatplotlibWidget, self).__init__(parent) # we can pass a figure but we can replot on it when
+        # pushing on a button (I didn't find a way to do it) while with the axes, you can still clear it and
+        # plot again on them
+        self.itight = itight
+        if figure is None:
+            figure = Figure()
+            self.canvas = FigureCanvasQTAgg(figure)
+            if threed is True:
+                axes = figure.add_subplot(111, projection='3d')
+            else:
+                axes = figure.add_subplot(111)
+        else:
+            axes = figure.get_axes()
+        self.figure = figure
+        self.axis = axes
+
+        self.layoutVertical = QVBoxLayout(self)
+        self.layoutVertical.addWidget(self.canvas)
+
+        if navi is True:
+            self.navi_toolbar = NavigationToolbar(self.canvas, self)
+            self.navi_toolbar.setMaximumHeight(30)
+            self.layoutVertical.addWidget(self.navi_toolbar)
+
+
+    def setMinMax(self, vmin=None, vmax=None):
+        coll = self.axis.collections[0]
+#        print('->', vmin, vmax)
+#        print('array ', coll.get_array())
+        if vmin is None:
+            vmin = np.nanmin(coll.get_array())
+        else:
+            vmin = float(vmin)
+        if vmax is None:
+            vmax = np.nanmax(coll.get_array())
+        else:
+            vmax = float(vmax)
+#        print(vmin, vmax)
+        coll.set_clim(vmin, vmax)
+        self.canvas.draw()
+
+
+    def plot(self, callback, threed=False):
+        ''' call a callback plot function and give it the ax to plot to
+        '''
+#        print('plot is called')
+        self.figure.clear() # need to clear the figure with the colorbar as well
+        if threed is False:
+            ax = self.figure.add_subplot(111)
+        else:
+            ax = self.figure.add_subplot(111, projection='3d')
+        self.callback = callback
+        callback(ax=ax)
+        if threed is False:
+            ax.set_aspect('auto')
+            ax.set_autoscale_on(False)
+        if self.itight is True:
+            self.figure.tight_layout()
+        self.canvas.draw()
+
+    def setCallback(self, callback):
+        self.callback = callback
+
+    def replot(self, threed=False, **kwargs):
+        self.figure.clear()
+        if threed is False:
+            ax = self.figure.add_subplot(111)
+        else:
+            ax = self.figure.add_subplot(111, projection='3d')
+        self.axis = ax
+        self.callback(ax=ax, **kwargs)
+        ax.set_aspect('auto')
+        if self.itight is True:
+            self.figure.tight_layout()
+        self.canvas.draw()
+
+    def clear(self):
+        self.axis.clear()
+        self.figure.clear()
+        self.canvas.draw()
+
+
+#%%
+class App(QMainWindow):
+    ''' Main UI application for emagpy. This call the main emagpy object for 
+    all the processing.
+    '''
+    def __init__(self, parent=None):
+        super().__init__()
+        
+        # do the checks for wine and updates in seperate thread
+#        tupdate = customThread(self.updateChecker)
+#        tupdate.signal.connect(self.updateCheckerShow)
+#        tupdate.start()
+        
+        self.setWindowTitle('EMagPy')
+        self.setGeometry(100,100,1100,600)
+
+        self.problem = None
+        self.datadir = os.path.join(bundle_dir, 'emagpy', 'test')
+
+        def errorDump(text, flag=1):
+            text = str(text)
+            timeStamp = time.strftime('%H:%M:%S')
+            if flag == 1: # error in red
+                col = 'red'
+            else:
+                col = 'black'
+            errorLabel.setText('<i style="color:'+col+'">['+timeStamp+']: '+text+'</i>')
+        errorLabel = QLabel('<i style="color:black">Error messages will be displayed here</i>')
+        QApplication.processEvents()
+
+
+        def infoDump(text):
+            errorDump(text, flag=0)
+
+        self.table_widget = QWidget()
+        layout = QVBoxLayout()
+        tabs = QTabWidget()
+        
+        
+        #%% tab 1 importing data
+        ''' STRUCTURE OF A TAB
+        - first create the tab widget and it's layout
+        - then create all the widgets that will interact in the tab
+        - at the end do the layouting (create sub layout if needed) and
+        '''
+
+        importTab = QTabWidget()
+        tabs.addTab(importTab, 'Importing')
+        
+        # import data
+        def importBtnFunc():
+            #TODO code when parsing the file
+            # create instance of emagpy
+            print('Hello well imported !')
+        importBtn = QPushButton('Import Data')
+        importBtn.clicked.connect(importBtnFunc)
+        
+        # selecte type of sensors
+        def sensorComboFunc(index):
+            print('sensor selected is:', sensors[index])
+        sensorCombo = QComboBox()
+        sensors = ['CMD Mini-Explorer (GF-Instruments)',
+                   'CMD Explorer (GF-Instruments)'
+                   ]
+        sensors = sorted(sensors)
+        sensors = ['Other'] + sensors
+        for sensor in sensors:
+            sensorCombo.addItem(sensor)
+        sensorCombo.currentIndexChanged.connect(sensorComboFunc)
+        
+        # display it
+        mwRaw = MatplotlibWidget()
+        
+                                   
+        
+        
+        # layout
+        importLayout = QVBoxLayout()
+        
+        topLayout = QHBoxLayout()
+        topLayout.addWidget(importBtn)
+        topLayout.addWidget(sensorCombo)
+        
+        importLayout.addLayout(topLayout)
+        importLayout.addWidget(mwRaw)
+        
+        importTab.setLayout(importLayout)
+        
+        
+        '''
+        TODO
+        - tab1: data import (choice of sensors)
+        - tab2: calibration + error model
+        - tab3: inversion settings
+            - model definition (layer, initial model)
+            - inversion (smoothing, lateral constrain, full or CS, choice of method for minimize)
+        - tab4: display of inverted section + export graph/data
+        - tab5: goodness of fit (1:1) and 2D graph
+        '''
+        
+        #%% calibration and error model
+        calibTab = QTabWidget()
+        tabs.addTab(calibTab, 'Calibration and error model')
+        
+        
+        
+        
+        
+        # layout
+        calibLayout = QVBoxLayout()
+        
+        calibTab.setLayout(calibLayout)
+
+        
+        #%% inversion settings
+        settingsTab = QTabWidget()
+        tabs.addTab(settingsTab, 'Inversion settings')
+        
+        
+        # layout
+        settingsLayout = QVBoxLayout()
+        
+        settingsTab.setLayout(settingsLayout)
+        
+        
+        #%% inverted section + export
+        invTab = QTabWidget()
+        tabs.addTab(invTab, 'Inversion')
+        
+        
+        # layout
+        invLayout = QVBoxLayout()
+        
+        invTab.setLayout(invLayout)
+        
+        
+        #%% goodness of fit
+        postTab = QTabWidget()
+        tabs.addTab(postTab, 'Post-processing')
+        
+        # layout
+        postLayout = QVBoxLayout()
+        
+        postTab.setLayout(postLayout)
+        
+        
+        #%% general Ctrl+Q shortcut + general tab layout
+
+        layout.addWidget(tabs)
+        layout.addWidget(errorLabel)
+        self.table_widget.setLayout(layout)
+        self.setCentralWidget(self.table_widget)
+        self.show()
+
+
+    def keyPressEvent(self, e):
+        if (e.modifiers() == Qt.ControlModifier) & (e.key() == Qt.Key_Q):
+            self.close()
+        if (e.modifiers() == Qt.ControlModifier) & (e.key() == Qt.Key_W):
+            self.close()
+
+#%% updater function and wine check
+    # based on https://kushaldas.in/posts/pyqt5-thread-example.html
+    def updateChecker(self): # check for new updates on gitlab
+        version = ResIPy_version
+        try:
+            versionSource = urlRequest.urlopen('https://gitlab.com/hkex/pyr2/raw/master/src/version.txt?inline=false')
+            versionCheck = versionSource.read().decode()
+            version = versionCheck.split()[1] # assuming version number is in 2nd line of version.txt
+            print('online version :', version)
+        except:
+            pass
+        return version
+    
+    def updateCheckerShow(self, version):
+        if ResIPy_version != version:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
+            msg.setText('''<b>ResIPy version %s is available</b>''' % (version))
+            msg.setInformativeText('''Please download the latest version of ResIPy at:<p><a href='https://gitlab.com/hkex/pyr2#gui-for-r2-family-code'>https://gitlab.com/hkex/pyr2</a></p><br>''')
+            msg.setWindowTitle("New version available")
+            bttnUpY = msg.addButton(QMessageBox.Yes)
+            bttnUpY.setText('Update')
+            bttnUpN = msg.addButton(QMessageBox.No)
+            bttnUpN.setText('Ignore')
+            msg.setDefaultButton(bttnUpY)
+            msg.exec_()
+            if msg.clickedButton() == bttnUpY:
+                webbrowser.open('https://gitlab.com/hkex/pyr2#gui-for-r2-family-code') # can add download link, when we have a direct dl link
+    
+
+
+#%% main function
+if __name__ == '__main__':
+    catchErrors()
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+    app.setWindowIcon(QIcon(os.path.join(bundle_dir, 'logo.png'))) # that's the true app icon
+    
+    # initiate splash screen when loading libraries    
+    splash_pix = QPixmap(os.path.join(bundle_dir, 'loadingLogo.png'))
+    splash = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
+    splash.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+    splash.setEnabled(False)
+    progressBar = QProgressBar(splash)
+    progressBar.setMaximum(10)
+    progressBar.setGeometry(100, splash_pix.height() - 50, splash_pix.width() - 200, 20)
+    splash.show()
+    splash.showMessage("Loading libraries", Qt.AlignBottom | Qt.AlignCenter, Qt.black)
+    app.processEvents()
+    progressBar.setValue(1)
+    app.processEvents()
+
+    # in this section all import are made except the one for pyQt
+    print('importing matplotlib')
+    from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+    from matplotlib.backends.backend_qt5 import NavigationToolbar2QT as NavigationToolbar
+    from matplotlib.figure import Figure
+    from matplotlib import rcParams
+    rcParams.update({'font.size': 13}) # CHANGE HERE for graph font size
+    progressBar.setValue(2)
+    app.processEvents()
+
+    print('importing numpy')
+    import numpy as np
+    progressBar.setValue(4)
+    app.processEvents()
+
+    print ('importing pandas')
+    import pandas as pd
+    progressBar.setValue(6)
+    app.processEvents()
+
+    print('importing python libraries')
+    from datetime import datetime
+    progressBar.setValue(8)
+    app.processEvents()
+
+    # library needed for update checker + wine checker
+    print('other needed libraries')
+    import platform
+    OS = platform.system()
+    from subprocess import PIPE, Popen
+#    from urllib import request as urlRequest
+#    import webbrowser
+
+    print('import emagpy')
+#    from resipy.R2 import R2
+#    from resipy.r2help import r2help
+#    splash.showMessage("ResIPy is ready!", Qt.AlignBottom | Qt.AlignCenter, Qt.black)
+    progressBar.setValue(10)
+    app.processEvents()
+
+    ex = App()
+    splash.hide() # hiding the splash screen when finished
+    
+    sys.exit(app.exec_())
