0dcc37431211bee659948c1359f68cf0a93841ca
Guillaum <sagitta1618@gmail.com>
2020-02-07 18:39:27 +0000

    fix test.py ??

--- src/test.py
+++ src/test.py
@@ -7,23 +7,23 @@ Created on Thu Jul 11 20:36:28 2019
 """
 
 from emagpy import Problem
-testdir = '../examples/'
+testdir = 'examples/'
 
 #%% importing from GF instrument
 k = Problem()
-k.importGF(testdir + 'coverCropLo.dat', testdir + 'coverCropHi.dat')
+k.importGF(testdir + 'cover-crop/coverCropLo.dat', testdir + 'cover-crop/coverCropHi.dat')
 k.show()
 
 #%% runnning mean
 k = Problem()
-k.createSurvey(testdir + 'coverCrop.csv')
+k.createSurvey(testdir + 'cover-crop/coverCrop.csv')
 k.showMap(contour=True)
 k.rollingMean()
 k.showMap(contour=True) # TODO segmentation fault
 
 #%% mapping the regolith of North Wyke
 k = Problem()
-k.createSurvey(testdir + 'regolith.csv')
+k.createSurvey(testdir + 'saprolite/regolith.csv')
 k.convertFromNMEA()
 k.showMap(contour=True, pts=True)
 k.show()
@@ -33,7 +33,7 @@ k.showMap(coil = k.coils[1])
 
 #%% inversion
 k = Problem()
-k.createSurvey(testdir + 'coverCrop.csv')
+k.createSurvey(testdir + 'cover-crop/coverCrop.csv')
 k.surveys[0].df = k.surveys[0].df[:10] # only keep 10 first measurements to make it faster
 k.invert(forwardModel='CS fast')
 k.showResults()
@@ -62,22 +62,22 @@ k.showOne2one()
 
 #%%
 k = Problem()
-k.createSurvey(testdir + 'coverCrop.csv')
+k.createSurvey(testdir + 'cover-crop/coverCrop.csv')
 k.invert(forwardModel='CS', alpha=0.1, beta=1)
 k.showResults()
 
 #%% mapping
 k = Problem()
-k.createSurvey('emagpy/test/regolith.csv')
+k.createSurvey(testdir + 'saprolite/regolith.csv')
 k.convertFromNMEA()
 k.invertGN()
 k.showSlice()
-k.saveMap(fname='emagpy/test/map.tiff', method='idw')
-k.saveSlice(fname='emagpy/test/slice.tiff')
+k.saveMap(fname=testdir + 'saprolite/map.tiff', method='idw')
+k.saveSlice(fname=testdir + 'saprolite/slice.tiff')
 
 #%% inverting iwith SCUEA
 k = Problem()
-k.createSurvey(testdir + 'coverCrop.csv')
+k.createSurvey(testdir + 'cover-crop/coverCrop.csv')
 k.surveys[0].df = k.surveys[0].df[:5]
 #k.invertMCMC()
 k.invertMCMCq(rep=500)
