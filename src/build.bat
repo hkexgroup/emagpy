d197fd2f3889d587cfb7ef2d576ec81f1b4fb981
Guillaum <sagitta1618@gmail.com>
2020-06-09 12:39:46 +0100

    update windows build

--- src/build.bat
+++ src/build.bat
@@ -3,7 +3,6 @@ pyinstaller -y ui-dir.spec && ^
 cd dist\ui && ^
 7z a -aoa -r ..\..\ui.zip * && ^
 cd ..\.. && ^
-move /y dist\ui.zip ui.zip && ^
 pyinstaller -y splashScreen-exe.spec && ^
 move /y dist\EMagPy-launch.exe dist\EMagPy-windows.exe && ^
 move /y ui.zip dist\EMagPy-windows.zip
