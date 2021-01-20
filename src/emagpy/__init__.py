6c11a49fe6177a10ab3927ed4ddd8125a88ab798
Guillaum <sagitta1618@gmail.com>
2021-01-21 00:22:31 +0100

    update to v1.2.0

--- src/emagpy/__init__.py
+++ src/emagpy/__init__.py
@@ -5,7 +5,7 @@ Created on Sun May  5 22:03:32 2019
 
 @author: jkl
 """
-EMagPy_version = '1.1.0'
+EMagPy_version = '1.2.0'
 name = 'emagpy'
 from emagpy.Problem import Problem
 from emagpy.Survey import Survey
