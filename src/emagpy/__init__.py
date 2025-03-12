e873fa31136b877b66e500df923fba4a944ed0dd
Guillaume Blanchy <guillaume.blanchy@ilvo.vlaanderen.be>
2025-03-12 19:37:28 +0100

    Update to v1.4.2

--- src/emagpy/__init__.py
+++ src/emagpy/__init__.py
@@ -5,7 +5,7 @@ Created on Sun May  5 22:03:32 2019
 
 @author: jkl
 """
-EMagPy_version = '1.4.1'
+EMagPy_version = '1.4.2'
 name = 'emagpy'
 from emagpy.Problem import Problem
 from emagpy.Survey import Survey
